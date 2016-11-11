e60d885584f5766ccc7b9070d39108ef0700f43f
“tanlin816” <1464133184@qq.com>
2016-11-12 07:42:51 +0800

    a code of local histograme equalization

--- Imageprocessing/Opencvexample/cmake/caper3/Histogramprocessing/localhistogramequalization/main.cpp
+++ Imageprocessing/Opencvexample/cmake/caper3/Histogramprocessing/localhistogramequalization/main.cpp
@@ -25,44 +25,54 @@ int main(int argc, char* argv[])
     	imshow("Original Image", src);
     	int r = src.rows;
   	int c = src.cols;
- 	int n = r*c;
+ 	int t = r*c;
   	Mat dst_2(r,c,CV_8U);
 	dst_2=src.clone();
  	//get the histogram of original image
   	uchar *p_1 = NULL;
+	uchar *p_2 = NULL;
  	unsigned int hist[256] = {0};
-	for(int m=1;m<r-1;m++)
-	{
-		for(int w=1;w<c-1;w++)
-		{
- 			for(int i=m-1;i<m+3;i++)
- 			{
-     				p_1 = src.ptr<uchar>(i);
-    				for(int j=w-1;j<w+3;j++)
-    				{
-         				hist[p_1[j]] = hist[p_1[j]]+1;//找出灰度值为j的像素点数
+	uchar transf_fun[256] = {0};//r的概率密度函数*255
+	int m=3,n=3,num=0;
+	for (int i = m / 2;i < r-m / 2;i++){
+		p_1 = src.ptr<uchar>(i);
+		p_2 = dst_2.ptr<uchar>(i);	
+		for (int j = n / 2 ;j < c-n / 2;j++){
+			for(int k = i - m / 2 ;k < i + m / 2;k++){
+				
+				for(int w = j - n / 2 ;w < j + n / 2;w++){
+         				hist[p_1[w]]= hist[p_1[w]]+1;//找出灰度值为j的像素点数
      				}
  			}
+			
+			if(j=n/2)
+			{	
+				for(int k = 0;k < 3;k++){
+				
+					for(int w = 1 ;w < 3;w++){
+         					hist[p_1[w]] = hist[p_1[w-1]]+hist[p_1[w]];
+						num=w;
+     					}
+ 				}
+				p_2[1] =  (uchar)(255*hist[p_1[num]]/t);
+			}
+			else
+			{	for(int k = i - m / 2 ;k < i + m / 2;k++){
+				
+					for(int w = j - n / 2 ;w < j + n / 2;w++){
+         					hist[p_1[w]] = hist[p_1[w-1]]+hist[p_1[w]];
+						num=w;
+     					}
+ 				}
+			
+				p_2[j] =  (uchar)(255*hist[p_1[num]]/t);
+			}
+					
   		}
-	}		//calculate the transform function
- 	uchar transf_fun[256] = {0};//r的概率密度函数*255
- 	transf_fun[0] = (uchar)(255*hist[0]/n);//0比较特殊
- 	for(int i=1;i<8;i++)
- 	{
-     		hist[i] = hist[i-1]+hist[i];
-     		transf_fun[i] = (uchar)(255*hist[i]/n);
- 	}
-    	//pad dst_2 the equalized values
- 	uchar *p_2 = NULL;
- 	for(int i=1;i<r-1;i++)
- 	{
-     		p_2 = dst_2.ptr<uchar>(i);
-     		p_1 = src.ptr<uchar>(i);
-     		for(int j=1;j<c-1;j++)
-     		{
-         		p_2[j] = transf_fun[p_1[j]];//对输入图像进行直方图均衡.
-     		}
- 	}
+	}	
+ 	
+
+    	
 
  	imshow("histogram equalization_own",dst_2) ;
 	imwrite("ww3.jpg",dst_2);
